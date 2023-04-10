0306be938647f5d3702e0b0b0d5e29c28c3c742b
Sebastian <sebastil@hotmail.com>
2023-04-10 23:35:07 +0800

    Initial setup

---
+++ synpulsebankapi/src/main/java/com/example/synpulsebankapi/SynpulsebankapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.synpulsebankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynpulsebankapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynpulsebankapiApplication.class, args);
+	}
+
+}
---
+++ synpulsebankapi/src/test/java/com/example/synpulsebankapi/SynpulsebankapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.synpulsebankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynpulsebankapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
